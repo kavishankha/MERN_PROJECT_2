90a59a83ef922b05174dc723446c661fe62fcefb
kavishankha <kavishankakodithuwakku12345@gmail.com>
2023-08-08 14:22:14 +0530

    create post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import ThumbUpAlt from '@mui/icons-material/ThumbUpAlt'
+import Delete from '@mui/icons-material/Delete'
+import MoreHoriz from '@mui/icons-material/MoreHoriz'
+import moment from 'moment';
 
-const Post = () => {
+
+const Post = ({ post,}) => {
+
+
     return (
-        <h1>Post</h1>
+        <Card>
+            <CardMedia  image={post.selectedFile}  title={post.title} />
+            <div >
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div >
+                <Button style={{ color: 'white' }} size="small" onClick={() =>{}}><MoreHoriz fontSize="default" /></Button>
+            </div>
+            <div >
+                <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+            </div>
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary"><ThumbUpAlt  fontSize="small" /> Like {post.likeCount} </Button>
+                <Button size="small" color="primary" ><Delete fontSize="small" /> Delete</Button>
+            </CardActions>
+        </Card>
     );
-}
-export default Post
+};
+
+export default Post;
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,7 @@
-const postMessage = require('../models/postMessage');
+const PostMessage = require('../models/postMessage');
  const getPosts =async (req, res)=>{
     try {
-        const postMessages= await postMessage.find()
+        const postMessages= await PostMessage.find()
         console.log(postMessages);
         res.status(200).json(postMessages);
     }

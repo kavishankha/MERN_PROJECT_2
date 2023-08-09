91f8c04465e337c8da42a43ca8864b1eac97518f
kavishankha <kavishankakodithuwakku12345@gmail.com>
2023-08-09 12:00:27 +0530

    erros fixed

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -4,10 +4,10 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
     try {
         // Make the API call to fetch posts
-        const { data } = await api.fetchPosts();
+        const {data} = await api.fetchPosts();
 
         // Dispatch an action with the retrieved data
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({type: 'FETCH_ALL', payload: data});
     } catch (e) {
         // Handle errors by logging the error message
         console.log(e.message);
@@ -15,9 +15,9 @@ export const getPosts = () => async (dispatch) => {
 };
 export const createPost = (post) => async (dispatch) => {
     try {
-        const { data } = await api.createPost(post);
+        const {data} = await api.createPost(post);
 
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({type: 'CREATE', payload: data});
     } catch (error) {
         console.log(error.message);
     }
